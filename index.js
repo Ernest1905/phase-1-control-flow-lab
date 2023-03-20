6032bb37454b56d69290ce37d50a3daf0f912a9e
Ernest <ernest.okiror@student.moringaschool.com>
2023-03-20 12:14:00 +0300

    Completedassignment

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <=400){
+    return('This one is on me!')
+  }  
+  
+  if (feet >= 2000 && feet <=2500){
+    return('I will gladly take your thirty bucks.')
+  }
+   if (feet > 2500){
+    return('No can do.')
+  }
 }
+function ternaryCheckCity(city){
+  return (city === "NYC") ? "Ok, sounds good." : "No go.";
+}
+function switchOnCharmFromTip(tipAmount){
+  switch(tipAmount) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
+}
+  
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  
+    
+  
+  
+    
+  
+  
+  
+      
+  
+     
+  
+  
+  
+     
+      
+        
+      
+
+       
+
+        
+
+  
+   
+    
+    
+  
+
+   
+
+   
+    
+   
+
+
+
+
+
+
